c170b79418de75d278cc8ac36ba645bc8a608bc5
alansinbailarin <alan_y_perla@hotmail.com>
2023-03-09 20:59:23 -0800

    Se subio

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -2,28 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Group;
 use Illuminate\Http\Request;
 
 class GroupController extends Controller
 {
 
     public function index(Request $request){
-
+        $group = Group::with("students","career")->get();
+        return Collect([
+            "process" => "select all groups",
+            "status" => "1",
+            "error" => "0",
+            "data" => $group
+        ]);
     }
 
-    public function show(Request $request){
+    public function show($id){
+        $group = Group::find($id);
 
+        return response()->json($group);
     }
 
     public function store(Request $request){
-
+        $group = Group::create([
+            "name" => $request['name'],
+            "grade" => $request['grade'],
+            "career_id" => $request['career_id'],
+        ]);
+        return response()->json($group);
     }
 
+    // update group by id
     public function update(Request $request){
-
+        $group = Group::find($request['id']);
+        $group->update($request->all());
+        return response()->json($group);
     }
 
+    // delete group by id and send message of deleted successfully
     public function destroy(Request $request){
+        $group = Group::find($request['id']);
         
+        $group->delete();
+        return response()->json("Group deleted successfully");
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\GroupController;
 
 
 /*
@@ -28,3 +29,10 @@ Route::post('/student/createStudent', [StudentController::class, 'store']);
 //Route::post('/student/createStudent/{serialNumber}/{profile_id}/{career_id}/{group_id}', [StudentController::class, 'store']);
 Route::post('/student/update', [StudentController::class, 'update']);
 Route::post('/student/destroy', [StudentController::class, 'destroy']);
+
+// groups routes
+Route::get('/groups', [GroupController::class, 'index']);
+Route::get('/group/id/{id}', [GroupController::class, 'show']);
+Route::post('/group/createGroup', [GroupController::class, 'store']);
+Route::put('/group/update/{id}', [GroupController::class, 'update']);
+Route::delete('/group/destroy/{id}', [GroupController::class, 'destroy']);
